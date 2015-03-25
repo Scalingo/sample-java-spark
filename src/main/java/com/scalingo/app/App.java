976af71a2d282b6c2999ac83e5e36608b877d569
Soulou <leo@unbekandt.eu>
2015-03-25 10:37:55 +0100

    PORT binding and procfile

--- src/main/java/com/scalingo/app/App.java
+++ src/main/java/com/scalingo/app/App.java
@@ -9,6 +9,11 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        int port = 4567;
+        if(System.getenv("PORT") != null) {
+            port = Integer.parseInt(System.getenv("PORT"));
+        }
+        setPort(port);
         get("/hello", (req, res) -> "Hello World");
     }
 }
