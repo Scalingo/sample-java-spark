07ebe49af07769cc8c2742e43aaf65a9080a1fcc
Étienne Michon <etienne@scalingo.com>
2018-02-05 17:20:33 +0100

    Improve html template

--- src/main/java/com/scalingo/app/App.java
+++ src/main/java/com/scalingo/app/App.java
@@ -5,15 +5,29 @@ package com.scalingo.app;
  * @version 2015/03/25
  */
 
+import java.util.*;
+import spark.template.velocity.*;
 import static spark.Spark.*;
 
 public class App {
-    public static void main(String[] args) {
-        int port = 4567;
-        if(System.getenv("PORT") != null) {
-            port = Integer.parseInt(System.getenv("PORT"));
-        }
-        setPort(port);
-        get("/hello", (req, res) -> "Hello World");
-    }
+	public static void main(String[] args) {
+			int port = 4567;
+			if(System.getenv("PORT") != null) {
+					port = Integer.parseInt(System.getenv("PORT"));
+			}
+			setPort(port);
+
+			staticFiles.location("/public");
+			staticFiles.expireTime(600L);
+
+			get("/", (req, res) -> renderHomepage(req));
+	}
+
+	private static String renderHomepage(Request req) {
+		return renderTemplate("velocity/index.vm", model);
+	}
+
+	private static String renderTemplate(String template, Map model) {
+		return new VelocityTemplateEngine().render(new ModelAndView(model, template));
+	}
 }
